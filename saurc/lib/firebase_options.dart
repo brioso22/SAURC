61520f08017c457893e66626e657d1705f2ee846
Francisco3652e <lemuscruzfranciscojavier@gmail.com>
2026-03-22 11:38:10 -0600

    arreglando anomalias

--- saurc/lib/firebase_options.dart
+++ saurc/lib/firebase_options.dart
@@ -1,13 +1,10 @@
 // ignore_for_file: type=lint
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
-/// Default [FirebaseOptions] para el proyecto SAURC.
-/// Este archivo fue corregido manualmente basándose en tu google-services.json.
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    if (kIsWeb) throw UnsupportedError('Web no configurada');
+    if (kIsWeb) return web; // Cambiado: ya no lanza error
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
@@ -15,6 +12,15 @@ class DefaultFirebaseOptions {
         throw UnsupportedError('Plataforma no soportada');
     }
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyDIQnzBFrlcKXJ_nAgM0IAZJYs9Aw68Dcc',
+    appId: '1:836945471405:web:ce4b2f2818228392e21245', // ID genérico, cámbialo si tienes el real
+    messagingSenderId: '836945471405',
+    projectId: 'saurc-42a13',
+    authDomain: 'saurc-42a13.firebaseapp.com',
+    storageBucket: 'saurc-42a13.firebasestorage.app',
+  );
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyDIQnzBFrlcKXJ_nAgM0IAZJYs9Aw68Dcc',
